0e6a25740bba8a589a083221e39e2eecd927ab59
Saloni Chouhan <chouhansaloni99@gmail.com>
2022-09-03 18:04:13 +0530

    Validation applied to Title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 	def new
+		@article = Article.new
 	end
 
     def index 
@@ -9,8 +10,11 @@ class ArticlesController < ApplicationController
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
-		redirect_to @article
+		if @article.save
+		    redirect_to @article
+		else
+			render 'new'
+	    end
 	end
     def show
     	@article = Article.find(params[:id])
